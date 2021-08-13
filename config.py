5570adceb6bf2da9cae4f2c5b82a9bcb05ac86cb
plawler92 <plawler92@gmail.com>
2021-08-13 11:08:29 -0500

    Update config.py
    removing keys

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ class Config:
     checks_path = os.getenv("CHECKS_PATH", default="newchecks/")
     s3_bucket = os.getenv("S3_BUCKET", default="echo-bi-datateam-dev")
     s3_folder = os.getenv("S3_FOLDER", default="checkrunner")
-    aws_access_key = os.getenv("AWS_ACCESS_KEY", default="AKIA5BPDHQI65MKEB746")
-    aws_secret_access_key = os.getenv("AWS_SECRET_KEY", default="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")
-    aws_role_arn = os.getenv("AWS_ROLE_ARN", default="arn:aws:iam::896514097725:role/echo-sec-dev-bi-datateam-checkrunner-role")
-    aws_external_id = os.getenv("AWS_EXTERNAL_ID", default="896514097725")
+    aws_access_key = os.getenv("AWS_ACCESS_KEY", default="")
+    aws_secret_access_key = os.getenv("AWS_SECRET_KEY", default="")
+    aws_role_arn = os.getenv("AWS_ROLE_ARN", default="")
+    aws_external_id = os.getenv("AWS_EXTERNAL_ID", default="")
 
     databases = {
         "TestDB": os.getenv("TestDB", default="DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=TestDB;UID=sa;PWD=Echo1234"),
